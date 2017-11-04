a468aade36c4ed26c2da18fc7b71b5ace7b27c28
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-11-04 12:05:14 +0100

    combine all DB logic into a class

--- db.py
+++ db.py
@@ -1,25 +1,47 @@
 from pymongo import MongoClient
-from pymongo.errors import OperationFailure
+from pymongo.errors import OperationFailure, ServerSelectionTimeoutError
 
-def connect(user, password, host, port, dbname):
-    client = MongoClient(host=host, port=port)
-    db     = client[dbname]
 
-    try:
-        db.authenticate(name=user, password=password)
-    except OperationFailure as e:
-        error = e.details["errmsg"]
-        message =  "Please ensure that the credentials passed are valid."
-        raise Exception("{} {}".format(error, message))
-    else:
-        return db
+class JobsDB(object):
 
-def insert_jobs(db, jobs):
+    def __init__(self, user, password, host, port, dbname):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = int(port)
+        self.dbname = dbname
 
-    try:
-        jobs_inserted = db.jobs.insert_many(jobs)
-    except Exception as e:
-        raise Exception("Something Happened {}".format(e))
-    else:
-        return jobs_inserted.inserted_ids
+    def connect(self):
 
+        # 1 - Establish a connection to the MongoDB server
+
+        try:
+            client = MongoClient(host=self.host, port=self.port)
+        except TypeError as e:
+            # This should be logged instead
+            print("Error: {}".format(e))
+            return
+        else:
+            self.db = client[self.dbname]
+
+        # 2 - Authenticate user using passed credentials
+
+        try:
+            self.db.authenticate(name=self.user, password=self.password)
+        except OperationFailure as e:
+            error = e.details["errmsg"]
+            message = "Please ensure that the credentials passed are valid."
+            raise Exception("{} {}".format(error, message))
+        except ServerSelectionTimeoutError as e:
+            print("Connection Error. Please check the host and port.")
+        else:
+            return self.db
+
+    def insert_jobs(self, jobs_list):
+
+        try:
+            jobs_inserted = self.db.jobs.insert_many(jobs_list)
+        except Exception as e:
+            raise Exception("Error inserting jobs. Reason: {}".format(e))
+        else:
+            return jobs_inserted.inserted_ids
