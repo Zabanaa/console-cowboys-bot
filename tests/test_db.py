72a9c5a45e7e074f43da9137d32c4e75f636768c
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-11-13 11:04:28 +0100

    add setup/teardown methods and test inser_jobs

--- tests/test_db.py
+++ tests/test_db.py
@@ -12,22 +12,27 @@ DB_PORT             = os.getenv("CONSOLE_COWBOYS_MONGO_PORT")
 DB_NAME             = os.getenv("CONSOLE_COWBOYS_MONGO_NAME")
 
 
-class TestDB():
+class TestDB(object):
+
+
+    def setup_method(self):
+        self.db = JobsDB(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, 2000)
+        self.connection = self.db.connect()
+
+    def teardown_method(self):
+        self.connection.client.close()
 
     def test_create_connection(self):
-
-        db = JobsDB(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, 2000)
-        connection = db.connect()
-        assert isinstance(connection, MongoDatabase)
-        assert DB_NAME == db.dbname == connection._Database__name
-
+        assert isinstance(self.connection, MongoDatabase)
+        assert DB_NAME == self.db.dbname == self.connection._Database__name
 
     def test_failed_connection(self):
-        # Invalid host and port
+
         with pytest.raises(SystemExit) as exc:
-            db = JobsDB("superman", "clark123",
+            # override self.db with invalid host and port information
+            self.db = JobsDB("superman", "clark123",
                         "1.2.3.4", 23454, "supermandb", 2000)
-            connection = db.connect()
+            self.connection = self.db.connect()
 
         exception_message = exc.value.args[0]
         expected_err_msg = "Connection Error. Please check the host and port."
@@ -35,11 +40,11 @@ class TestDB():
 
 
     def test_failed_authentication(self):
-        # Invalid password
         with pytest.raises(SystemExit) as exc:
-            db = JobsDB(DB_USER, "clark123",
+            # override self.db with invalid password
+            self.db = JobsDB(DB_USER, "clark123",
                         DB_HOST, DB_PORT, DB_NAME, 2000)
-            connection = db.connect()
+            self.connection = self.db.connect()
 
         exception_message = exc.value.args[0]
         expected_err_msg = "Please ensure credentials are valid."
@@ -48,4 +53,11 @@ class TestDB():
 
 
     def test_insert_jobs(self):
+        jobs = [{"hello": "world"}, {"name": 123}]
+        inserted_jobs = self.db.insert_jobs(jobs, "new-york")
+
+        assert len(inserted_jobs) == len(jobs)
+
+    def test_insert_jobs_fail(self):
+        # find a way to make the bloody test fail
         pass
